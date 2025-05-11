d2386a8b6f777cf77ad26dd0afeb1d68e89a94c6
tigisthailay <tigisthailay13@gmail.com>
2025-05-11 17:19:50 +0300

    kafka setup removed

--- kafka/scripts/kafka-setup.sh
+++
@@ -1,8 +0,0 @@
-# blocks until kafka is reachable
-echo -e 'Creating kafka topics'
-kafka-topics --bootstrap-server broker:29092 --create --topic FinnhubTrade --if-not-exists --replication-factor 1 --partition 1
-
-#TOPIC THAT HOLDS DATA EXPOSED BACK FROM SPARK 
-kafka-topics --bootstrap-server broker:29092 --create --topic FinnhubTradeback --if-not-exists --replication-factor 1 --partition 1
-echo -e 'Successfully created the following topics:'
-kafka-topics --bootstrap-server localhost:29092 --list
